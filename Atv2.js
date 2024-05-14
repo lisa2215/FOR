f9c8377e645b2b7df55085bd3c2c6e41d5181c45
lisa <160615135+lisa2215@users.noreply.github.com>
2024-05-14 09:17:52 -0300

    Add files via upload

--- Atv2.js
+++ Atv2.js
@@ -1,11 +1,9 @@
-var soma = new Array(20, 30, 40, 50, 60, 70, 80, 90, 100, 110);
-console.log("10 + 10 = ".concat(soma[0]));
-console.log("10 + 20 = ".concat(soma[1]));
-console.log("10 + 30 = ".concat(soma[2]));
-console.log("10 + 40 = ".concat(soma[3]));
-console.log("10 + 50 = ".concat(soma[4]));
-console.log("10 + 60 = ".concat(soma[5]));
-console.log("10 + 70 = ".concat(soma[6]));
-console.log("10 + 80 = ".concat(soma[7]));
-console.log("10 + 90 = ".concat(soma[8]));
-console.log("10 + 100 = ".concat(soma[9]));
+/* 2 - exibir os numeros pares de 0 a 60, fazer o uso do laço
+de repetição for
+*/
+console.clear();
+for (var par = 0; par <= 60; par++) {
+    if (par % 2 == 0) {
+        console.log("Os numero pares de 0 a 60 s\u00E3o: ".concat(par));
+    }
+}
--- Atv2.ts
+++ Atv2.ts
@@ -1,12 +1,12 @@
-let soma = new Array (20 , 30 , 40 , 50 , 60 , 70 , 80 , 90 , 100 , 110)
+/* 2 - exibir os numeros pares de 0 a 60, fazer o uso do laço 
+de repetição for 
+*/
 
-console.log(`10 + 10 = ${soma[0]}`);
-console.log(`10 + 20 = ${soma[1]}`);
-console.log(`10 + 30 = ${soma[2]}`);
-console.log(`10 + 40 = ${soma[3]}`);
-console.log(`10 + 50 = ${soma[4]}`);
-console.log(`10 + 60 = ${soma[5]}`);
-console.log(`10 + 70 = ${soma[6]}`);
-console.log(`10 + 80 = ${soma[7]}`);
-console.log(`10 + 90 = ${soma[8]}`);
-console.log(`10 + 100 = ${soma[9]}`);
+console.clear();
+
+for (let par=0; par <=60; par++){
+    if(par % 2 == 0){
+        console.log(`Os numero pares de 0 a 60 são: ${par}`);
+    }
+}
+
